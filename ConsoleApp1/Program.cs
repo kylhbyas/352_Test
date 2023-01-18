462196dccf56e14df8d920896ad26ca0470de893
kylhbyas <kylhbyas@EPSG1CL-825056>
2023-01-17 20:32:09 -1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private string thing;
+        public void Setter(string s)
+        {
+            thing = s;
+        }
+        public string Getter()
+        {
+            return thing;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Program p = new Program();
+            string r;
+            r = Console.ReadLine();
+
+            p.Setter(r);
+            Console.WriteLine(p.Getter());
+            Console.ReadKey();
+        }
+    }
+}
